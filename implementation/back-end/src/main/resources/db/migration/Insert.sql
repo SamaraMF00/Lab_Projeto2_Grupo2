54888046c705b89653f6726c6621339f7c0f927e
MatheusMOAndrade <andradematheusmo@gmail.com>
2023-04-03 21:29:04 -0300

    vehicle database

---
+++ implementation/back-end/src/main/resources/db/migration/Insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO vehicle (
+    id,
+    licensePlate,
+    model,
+    brand,
+    year,
+    owner
+) VALUES (
+    ("1", "AAA0000", "Gol 1.6", "Volkswagen", "2022", "Client"), 
+    ("2", "BBB1111", "Argo 1.0", "FIAT", "2021", "Bank"),
+    ("3", "CCC2222", "HB20 1.0", "Hyundai", "2022", "Enterprise"),
+    ("4", "DDD3333", "Gol 1.6", "Volkswagen", "2022", "Bank"), 
+    ("5", "EEE4444", "Argo 1.0", "FIAT", "2022", "Enterprise"),
+    ("6", "FFF5555", "HB20 1.0", "Hyundai", "2022", "Client"),
+    ("7", "GGG6666", "HB20 1.6", "Hyundai", "2023", "Bank")
+);
--- implementation/back-end/src/main/resources/db/migration/V1.0__CreateTables.sql
+++ implementation/back-end/src/main/resources/db/migration/V1.0__CreateTables.sql
@@ -5,5 +5,13 @@ CREATE TABLE customer (
                        rg VARCHAR(15) NOT NULL,
                        address VARCHAR(100) NOT NULL unique,
                        employers VARCHAR(100) NOT NULL
+);
 
+CREATE TABLE vehicle (
+                       id INT auto_increment PRIMARY KEY,
+                       licensePlate VARCHAR(7) NOT NULL,
+                       model VARCHAR(100) NOT NULL,
+                       brand VARCHAR(20) NOT NULL,
+                       year VARCHAR(4) NOT NULL,
+                       owner VARCHAR(100) NOT NULL
 );
