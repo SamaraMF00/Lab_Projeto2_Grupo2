f774494d90e8444879e734dccccab6c65f22bf99
MatheusMOAndrade <andradematheusmo@gmail.com>
2023-04-05 18:20:48 -0300

    vehicle request controller and service

--- implementation/back-end/src/main/java/vehicleRental/controller/VehicleRequestController.java
+++ implementation/back-end/src/main/java/vehicleRental/controller/VehicleRequestController.java
@@ -1,7 +1,61 @@
 package vehicleRental.controller;
 
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import vehicleRental.model.VehicleRequest;
+import vehicleRental.service.VehicleRequestService;
+
+@RestController
+@RequestMapping("/vehicleRequest")
+@Validated
+
 public class VehicleRequestController {
 
-    
+    @Autowired
+    private VehicleRequestService vehicleRequestService;
+
+    private URI getPatchId(Long id){
+        return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+    }
+
+    @PostMapping
+    @Validated
+    public ResponseEntity<Void> create(@Valid @RequestBody VehicleRequest obj){
+        VehicleRequest newVehicleRequest = this.vehicleRequestService.create(obj);
+        return ResponseEntity.created(getPatchId(newVehicleRequest.getId())).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VehicleRequest> findById(@PathVariable Long id){
+        VehicleRequest newVehicleRequest = this.vehicleRequestService.findById(id);
+        return ResponseEntity.ok().body(newVehicleRequest);
+    }
+
+    @GetMapping("/loanId/{loan_id}")
+    public ResponseEntity<List<VehicleRequest>> findAllByLoan(@PathVariable Long loan_id){
+        List<VehicleRequest> list = this.vehicleRequestService.findAllByLoan(loan_id);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.vehicleRequestService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- implementation/back-end/src/main/java/vehicleRental/service/VehicleRequestService.java
+++ implementation/back-end/src/main/java/vehicleRental/service/VehicleRequestService.java
@@ -1,5 +1,47 @@
 package vehicleRental.service;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vehicleRental.model.VehicleRequest;
+import vehicleRental.repository.VehicleRequestRepository;
+import vehicleRental.service.exceptions.DataBindingViolationException;
+import vehicleRental.service.exceptions.ObjectNotFoundException;
+
+@Service
 public class VehicleRequestService {
 
+    @Autowired
+    private VehicleRequestRepository vehicleRequestRepository;
+
+    public VehicleRequest findById(Long id) {
+        Optional<VehicleRequest> vehicleRequest = this.vehicleRequestRepository.findById(id);
+        return vehicleRequest.orElseThrow(() -> new ObjectNotFoundException("Vehicle request not found in database"));
+    }
+
+    @Transactional
+    public VehicleRequest create(VehicleRequest newVehicleRequest) {
+        newVehicleRequest.setId(null);
+        return this.vehicleRequestRepository.save(newVehicleRequest);
+    }
+
+    public void delete(Long id) {
+        try {
+            VehicleRequest newObj = findById(id);
+            this.vehicleRequestRepository.delete(newObj);
+        } catch (Exception e) {
+            throw new DataBindingViolationException("Entity request depends on another entity");
+        }
+    }
+
+    public void deleteAllByLoan(Long id){
+      this.vehicleRequestRepository.deleteById(id);       
+    }
+
+    public List<VehicleRequest> findAllByLoan(Long loan_id) {
+        List<VehicleRequest> vehicleRequest = this.vehicleRequestRepository.findAllByIdLoan(loan_id);
+        return vehicleRequest;
+    }
 }
